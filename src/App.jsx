7155ef455e2cafb816fefa3743ec6c8ec3e95613
Pedro Dayer <pedrob.dayer@gmail.com>
2026-06-29 13:13:15 -0300

    feat: inplementacao da funcionalidade de upload de imagem + redirecionamento + estilizacao inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import Resultado from './pages/resultado'
+import React, { useState } from 'react'
+import Resultado from './pages/Resultado'
+import UploadPage from './pages/Upload'
 
 export const App = () => {
+  const [imagemEnviada, setImagemEnviada] = useState(null)
+
+  const handleImageSubmit = (base64) => {
+    // Aqui você pode enviar a imagem para o n8n no futuro
+    setImagemEnviada(base64)
+  }
+
   return (
-    <Resultado></Resultado>
+    <>
+      {!imagemEnviada ? (
+        <UploadPage onImageSubmit={handleImageSubmit} />
+      ) : (
+        <Resultado imagemPreCarregada={imagemEnviada} />
+      )}
+    </>
   )
 }
+
+export default App
